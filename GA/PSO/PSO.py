06264f94a6bcad550bca11440102106cb1eef2d0
VictoriaCapovilla <viki@deicapovilla.it>
2024-11-30 13:45:32 +0100

    PSO

---
+++ GA/PSO/PSO.py
@@ -0,0 +1,38 @@
+import numpy as np
+import torch
+
+from fstpso import FuzzyPSO
+
+from GA.PSO.lower_level import LowerTorch
+
+
+class GeneticAlgorithmTorch:
+
+    def __init__(self, instance, lower_eps=10**(-12), device=None, reuse_p=False):
+
+        self.device = device
+        self.instance = instance
+
+        self.n_paths = self.instance.n_paths
+
+        self.M = (self.instance.travel_time[:, -1] * (
+                1 + self.instance.alpha * (self.instance.n_users / self.instance.q_od) ** self.instance.beta)).max()
+
+        self.lower = LowerTorch(self.instance, lower_eps, device=device, M=self.M, reuse_p=reuse_p)
+
+        self.obj_val = 0
+
+    def fitness_evaluation(self, population):
+        population = torch.repeat_interleave(torch.from_numpy(np.array(population)).unsqueeze(0),
+                                             repeats=self.instance.n_od, dim=0)
+        return - self.lower.eval(torch.from_numpy(np.array(population)))
+
+    def run_PSO(self):
+        dims = self.n_paths
+        FP = FuzzyPSO()
+        FP.set_search_space([[0, self.M]] * dims)
+        FP.set_fitness(self.fitness_evaluation)
+        result = FP.solve_with_fstpso()
+        print("Best solution:", result[0])
+        print("Whose fitness is:", np.abs(torch.from_numpy(result[1])))
+        self.obj_val = np.abs(torch.from_numpy(result[1]))
---
+++ GA/PSO/main.py
@@ -0,0 +1,70 @@
+import time
+
+import numpy as np
+import random
+
+import torch
+
+import pandas as pd
+
+from GA.PSO.PSO import GeneticAlgorithmTorch
+from Instance.instance import Instance
+
+seed = 0
+np.random.seed(seed)
+random.seed(seed)
+torch.manual_seed(seed)
+
+PATHS = 10
+N_OD = 4
+POP_SIZE = 8
+LOWER_EPS = 10**(-4)
+OFFSPRING_RATE = 0.5
+
+N_RUN = 6
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+instance = Instance(n_paths=PATHS, n_od=N_OD)
+
+total_df = None
+
+for i in range(N_RUN):
+    t = time.time()
+    genetic_algorithm = GeneticAlgorithmTorch(instance, lower_eps=LOWER_EPS, device=device, reuse_p=None)
+
+    genetic_algorithm.run_PSO()
+
+    # creating dataframe
+    data = {
+        'time': time.time() - t,
+        'upper_iter': 100,
+        'fitness': float(genetic_algorithm.obj_val),
+        'best_individual': [genetic_algorithm.lower.data_individuals[-1]],
+        'lower_time': [genetic_algorithm.lower.data_time],
+        'lower_iter': [genetic_algorithm.lower.n_iter],
+        'fit_update': [genetic_algorithm.lower.data_fit],
+        'ind_update': [genetic_algorithm.lower.data_individuals],
+        'n_paths': genetic_algorithm.n_paths,
+        'n_od': genetic_algorithm.instance.n_od,
+        'n_users': [genetic_algorithm.instance.n_users],
+        # 'pop_size': genetic_algorithm.pop_size,
+        'alpha': genetic_algorithm.instance.alpha,
+        'beta': genetic_algorithm.instance.beta,
+        'M': genetic_algorithm.M,
+        'K': float(genetic_algorithm.lower.K),
+        'eps': genetic_algorithm.lower.eps,
+        'run': i
+            }
+
+    df = pd.DataFrame(data=data)
+
+    if total_df is None:
+        total_df = df
+    else:
+        total_df = pd.concat([total_df, df])
+
+    print('run', i, 'complete', '\ntime:', time.time() - t, '\nfitness:', genetic_algorithm.obj_val)
+
+total_df.to_csv(r'GA\Project\PSO\study', index=False)
+df = pd.read_csv(r'C:\Users\viki\Desktop\NPP\GA\Project\PSO\study')
