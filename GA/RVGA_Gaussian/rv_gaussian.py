31e8ea78d63f2179484c69a49c04f69024a12489
capovilla <viki@deicapovilla.it>
2024-12-27 17:39:27 +0100

    update rv_gaussian

--- GA/RVGA_Gaussian/rv_gaussian.py
+++ GA/RVGA_Gaussian/rv_gaussian.py
@@ -1,3 +1,4 @@
+import random
 import time
 
 import numpy as np
@@ -7,10 +8,12 @@ import torch
 from GA.lower_level_torch import LowerTorch
 
 
-class GeneticAlgorithmTorch:
+class RVGA_Gaussian:
 
-    def __init__(self, instance, pop_size, offspring_proportion=0.5, lower_eps=10**(-12), device=None, reuse_p=False):
+    def __init__(self, instance, pop_size, offspring_proportion=0.5, lower_eps=10**(-12),
+                 device=None, reuse_p=False, save=False):
 
+        self.save = save
         self.device = device
         self.instance = instance
 
@@ -18,62 +21,67 @@ class GeneticAlgorithmTorch:
 
         self.pop_size = pop_size
         self.n_children = int(pop_size * offspring_proportion)
+        self.n_parents = self.n_children * 2
         self.mat_size = self.pop_size + self.n_children
-        # self.mask = torch.zeros(self.n_paths * self.n_children, device=self.device, dtype=torch.bool)
 
         self.M = (self.instance.travel_time[:, -1] * (
                 1 + self.instance.alpha * (self.instance.n_users / self.instance.q_od) ** self.instance.beta)).max()
 
-        self.lower = LowerTorch(self.instance, lower_eps, mat_size=self.mat_size, device=device, M=self.M, reuse_p=reuse_p)
+        self.lower = LowerTorch(self.instance, lower_eps, mat_size=self.mat_size, device=device, M=self.M, reuse_p=reuse_p, save=save)
 
         # initialization
         self.population = torch.rand(size=(self.mat_size, self.n_paths), device=self.device) * self.M
-        self.parents_idxs = torch.tensor([(i, j) for j in range(self.pop_size) for i in range(j + 1, self.pop_size)],
-                                         device=self.device)
-
-        self.parents = self.parents_idxs[torch.randperm(self.parents_idxs.shape[0])[:self.n_children]]
+        self.pop_idxs = torch.tensor([i for i in range(self.mat_size)], device=self.device)
 
         # fitness evaluation
         self.vals = torch.zeros(self.mat_size, device=self.device)
         self.vals = self.lower.eval(self.population)
 
-        self.data_fit = []
-        self.data_fit.append(float(self.vals[np.argsort(-self.vals.to('cpu'))][0]))
+        if self.save:
+            self.data_fit = []
+            self.data_fit.append(float(self.vals[np.argsort(-self.vals.to('cpu'))][0]))
 
-        self.data_individuals = []
-        self.data_individuals.append(self.population[0].detach().cpu().numpy())
+            self.data_individuals = []
+            self.data_individuals.append(self.population[np.argsort(-self.vals.to('cpu'))][0].detach().cpu().numpy())
 
-        self.obj_val = 0
+            self.times = []
 
-    def run(self, iterations):
-        for _ in range(iterations):
-            # SELECTION
-            self.parents = self.parents_idxs[torch.randperm(self.parents_idxs.shape[0])[:self.n_children]]
+    def tournament_selection(self, pop):
+        # select randomly n_parents individuals from the pop
+        tournament = torch.randperm(self.pop_idxs.shape[0], device=self.device)[:self.n_parents]
+        # find the individual with the max fit among the selected
+        fit = self.vals[tournament]
+        selected = pop[torch.argmax(fit)]
+        return selected
+
+    def crossover(self, parents):
+        weights = torch.rand(size=(self.n_children, self.n_paths), device=self.device)
+        children = weights * parents[:self.n_children] + (1 - weights) * parents[self.n_children:]
+        return children
+
+    def mutation(self, pop):
+        std, mean = torch.std_mean(pop, dim=1)
+
+        # create gaussian noise tensor
+        noise = torch.repeat_interleave((torch.normal(mean, std)).unsqueeze(0), repeats=self.instance.n_paths, dim=0).T
+        mutation = pop + noise
+        # bounds check
+        torch.where(mutation < 0, 0, mutation)
+        torch.where(mutation > self.M, self.M, mutation)
+        return mutation
+
+    def run_gm(self, generations):
+        if self.save:
+            self.times.append(time.time())
+        for _ in range(generations):
+            # TOURNAMENT SELECTION
+            selected = torch.stack([self.tournament_selection(self.population) for _ in range(0, self.n_parents)])
 
             # CROSSOVER:
-            weights = torch.rand(size=(self.n_children, self.n_paths), device=self.device)
-            self.population[self.pop_size:] = \
-                (weights * self.population[self.parents[:self.n_children][:, 0]]
-                 + (1 - weights) * self.population[self.parents[:self.n_children][:, 1]])
+            self.population[self.pop_size:] = self.crossover(selected)
 
             # GAUSSIAN MUTATION
-            # mean = torch.mean(self.population[self.pop_size:])
-
-            # sp = torch.mean((self.population[self.pop_size:])) ** 2     # Signal Power
-            # snr = 18                                                    # Signal-to-noise ratio
-            # std = (sp / snr) ** 0.5
-
-            # ostd = 0.5 * torch.std(self.population[self.pop_size:])
-
-            # std =
-
-            noise = torch.normal(0, std)
-
-            self.population[self.pop_size:] = self.population[self.pop_size:] + noise
-            torch.where(self.population[self.pop_size:] < 0, 0, self.population[self.pop_size:])
-            torch.where(self.population[self.pop_size:] > self.M, self.M, self.population[self.pop_size:])
-
-            # # valutare mutazione su tutta la popolazione
+            self.population[self.pop_size:] = self.mutation(self.population[self.pop_size:])
 
             # FITNESS EVALUATION
             self.vals = self.lower.eval(self.population)
@@ -81,15 +89,13 @@ class GeneticAlgorithmTorch:
             self.population = self.population[fitness_order]
             self.vals = self.vals[fitness_order]
 
-            # # sistemare elitismo
-            # idea: salvare i nuovi self.population[self.pop_size:] a parte e tenere i migliori self.mat_size tra
-            # questi e la vecchia popolazione
+            if self.save:
+                self.data_individuals.append(self.population[0].detach().cpu().numpy())
+                self.data_fit.append(float(self.vals[0]))
 
-            self.data_individuals.append(self.population[0].detach().cpu().numpy())
-            self.data_fit.append(float(self.vals[0]))
-            # print(self.vals[0])
+        if self.save:
+            self.times += self.lower.total_time
+            self.times = np.array(self.times)
+            self.times = list(self.times - self.times[0])
 
-        self.obj_val = self.vals[0]
-
-        # print('costs =\n', self.population[0])
-        # print('fitness =\n', self.vals[0])
+        return self.vals[0]
