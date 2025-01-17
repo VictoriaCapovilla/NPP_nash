8a32a481177880a56875bdedb9f4faf20b1f8172
andrea <andygaspar@libero.it>
2025-01-17 11:16:07 +0100

    bug fixed

--- debug.py
+++ debug.py
@@ -35,7 +35,7 @@ torch.manual_seed(seed)
 
 PATHS = 5
 N_OD = 5
-POP_SIZE = 1 #int(10 + 2*np.sqrt(PATHS))
+POP_SIZE = 5 #int(10 + 2*np.sqrt(PATHS))
 LOWER_EPS = 10**(-4)
 GENERATIONS = 20
 OFFSPRING_RATE = 0.5
@@ -49,28 +49,29 @@ N_RUN = 30
 
 SAVE = True
 total_df = None
+VERBOSE = False
 n = 0
 
 t = time.time()
 
 # vanilla genetic algorithm
-# genetic_algorithm = GeneticAlgorithmTorch(instance, POP_SIZE, lower_eps=LOWER_EPS,
-#                                           offspring_proportion=OFFSPRING_RATE, mutation_rate=MUTATION_RATE, device=device,
-#                                           reuse_p=None, save=True)
-# best_fit = genetic_algorithm.run(GENERATIONS, verbose=True)
+genetic_algorithm = GeneticAlgorithmTorch(instance, POP_SIZE, lower_eps=LOWER_EPS,
+                                          offspring_proportion=OFFSPRING_RATE, mutation_rate=MUTATION_RATE, device=device,
+                                          reuse_p=None, save=True)
+best_fit = genetic_algorithm.run(GENERATIONS, verbose=VERBOSE)
+
+cmaes = CMAES(instance, lower_eps=LOWER_EPS)
+cmaes.run_CMA(GENERATIONS, pop_size=POP_SIZE, verbose=VERBOSE)
+
+
+# x = np.random.uniform(low=0, high=150, size=(POP_SIZE, N_OD))
+# lower_torch = LowerTorch(instance, eps=LOWER_EPS, mat_size=POP_SIZE, device=device, M=113, save=False)
+# vals = lower_torch.eval(torch.tensor(x))
+# print(vals)
 #
-# cmaes = CMAES(instance, lower_eps=LOWER_EPS)
-# cmaes.run_CMA(GENERATIONS, pop_size=POP_SIZE, verbose=True)
-
-
-x = np.random.uniform(low=0, high=150, size=(POP_SIZE, N_OD))
-lower_torch = LowerTorch(instance, eps=LOWER_EPS, mat_size=POP_SIZE, device=device, M=113, save=False)
-vals = lower_torch.eval(torch.tensor(x))
-print(vals)
-
-# [5411.3747, 6195.0278, 5854.5367, 6569.0846, 5095.6198, 1273.4881],
-lower = LowerLevel(instance, M=113, eps=LOWER_EPS)
-print([lower.eval(x) for x in x])
+# # [5411.3747, 6195.0278, 5854.5367, 6569.0846, 5095.6198, 1273.4881],
+# lower = LowerLevel(instance, M=113, eps=LOWER_EPS)
+# print([lower.eval(x) for x in x])
 
 # real valued genetic algorithm with UNIFORM MUTATION
 # genetic_algorithm = RVGA_Uniform(instance, POP_SIZE, lower_eps=LOWER_EPS,
